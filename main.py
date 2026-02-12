12b731e605f2f9d4314bb80fc90e0146695f6120
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-02-12 13:34:01 +0500

    added common.py fifile in utils and added schema for the data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.diabeties import logger
 from src.diabeties.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.diabeties.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-from src.diabeties.pipeline.data_ingestion_pipeline import DataTransformationTrainingPipeline
+from src.diabeties.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.diabeties.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.diabeties.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
@@ -13,29 +13,29 @@ except Exception as e:
         raise e
 
 try:
-   data_ingestion = DataValidationTrainingPipeline()
-   data_ingestion.initiate_data_validation()
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
 except Exception as e:
         logger.exception(e)
         raise e
 
 try:
-   data_ingestion = DataTransformationTrainingPipeline()
-   data_ingestion.initiate_data_transformation()
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.initiate_data_transformation()
 except Exception as e:
         logger.exception(e)
         raise e
 
 try:
-   data_ingestion = ModelTrainerTrainingPipeline()
-   data_ingestion.initiate_model_training()
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.initiate_model_training()
 except Exception as e:
         logger.exception(e)
         raise e
 
 try:
-   data_ingestion = ModelEvaluationTrainingPipeline()
-   data_ingestion.initiate_model_evaluation()
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
 except Exception as e:
         logger.exception(e)
         raise e
